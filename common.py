e1d9094350ecacfd1303799727f4ba2f4062b7af
william-z <alan_zhou@qq.com>
2015-11-08 17:43:28 +0800

    update

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ import os
 import numpy
 
 from numpy import *
+from pylab import *
 from PIL import Image
 
 
@@ -14,8 +15,17 @@ def image_resize(image, new_size):
     return array(pil_im.resize(new_size))
 
 def histeq(image, nbr_bins=256):
+    imhist, bins = histogram(image.flatten(), nbr_bins, normed=True)
+    cdf = imhist.cumsum()
+    cdf = 255 * cdf / cdf[-1]
+    im2 = interp(image.flatten(), bins[:-1], cdf)
+    return im2.reshape(image.shape), cdf
 
 
 if __name__ == '__main__':
-    pass
+    im = array(Image.open(r'p:\picture\bridge_city_1-wallpaper-1920x1080.jpg').convert('L'))
+    im2, cdf = histeq(im)
+    imshow(im)
+    show()
+
 
